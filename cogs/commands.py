4cb08889afb21ba121f186d27341175b53ec95ac
Temriel <60169590+Temriel@users.noreply.github.com>
2025-02-14 01:05:54 +0100

    added /canvas command
    there are almost 900 files in the file this gets its shit from lmfao

--- cogs/commands.py
+++ cogs/commands.py
@@ -1,6 +1,7 @@
 import discord
 from discord import app_commands
 from discord.ext import commands
+import config
 
 class commander(commands.Cog):
     def __init__(self, client):
@@ -18,6 +19,37 @@ class commander(commands.Cog):
     async def ping(self, interaction: discord.Interaction):
         bot_latency = round(self.client.latency * 1000)
         await interaction.response.send_message(f'Pong! Current ping is {bot_latency}ms.')
+
+    @app_commands.command(name='canvas', description='See a canvas.')
+    @app_commands.describe(
+        canvas='Canvas number',
+        display='Choose whether to use a custom display or not.'
+    )
+    @app_commands.choices(display=[
+        app_commands.Choice(name='Initial', value='initial'),
+        app_commands.Choice(name='Final', value='final'),
+        app_commands.Choice(name='Activity', value='activity'),
+        app_commands.Choice(name='Age', value='age'),
+        app_commands.Choice(name='Virgin', value='virgin'),
+        app_commands.Choice(name='Milliseconds', value='milliseconds'),
+        app_commands.Choice(name='Minutes', value='minutes'),
+        app_commands.Choice(name='Seconds', value='seconds'),
+        app_commands.Choice(name='Combined', value='combined'),
+        ]
+    )
+    async def canvas(self, interaction: discord.Interaction, canvas: str, display: app_commands.Choice[str] = None):
+        displayed = display.value if display else 'final'
+        await interaction.response.defer(ephemeral=False, thinking=True)
+        try:
+            filename = f'canvas-{canvas}_{displayed}.png'
+            path = f'{config.pxlslog_explorer_dir}/pxls-final-canvas/canvas-{canvas}-{displayed}.png'
+            file = discord.File(path, filename=filename)
+            embed = discord.Embed(title=f'Canvas {canvas}, {displayed}.', )
+            embed.set_image(url=f'attachment://{filename}')
+            await interaction.followup.send(embed=embed, file=file)
+        except Exception as e:
+            await interaction.followup.send(f'Log files are... weird for c6, c17, c28, and c30a. If this error occured when trying to view those canvases, Tem knows about it already xd. If it was unrelated to those four, please ping Temriel!', ephemeral=True)
+            print(f'An error occurred: {e}')
 
 async def setup(client):
     await client.add_cog(commander(client))
